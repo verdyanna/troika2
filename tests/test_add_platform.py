576ffd4fd25f5675f8e73fd1d82c42f55490af8c
Anna <verdyanna@gmail.com>
2016-06-28 19:02:16 +0300

    2

--- tests/test_add_platform.py
+++ tests/test_add_platform.py
@@ -1,3 +1,6 @@
+
+
 def test_test_add_platform(app):
+
     app.session.login(username="admin@rsso.ru", password="bigthree3")
     app.platform_helper.fill_new_platform_form()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,7 @@
+import pytest
+
 def test_login(app):
-    app.session.login("admin@rsso.ru", "bigthree3")
+    with pytest.allure.step('First step'):
+        app.session.login("admin@rsso.ru", "bigthree3")
+
    # assert app.session.is_logged_in_as("admin@rsso.ru")
